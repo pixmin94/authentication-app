de5ee2073acf8af51cb738ee7c832339d409ecec
pixmin94 <pixmin@outlook.com>
2022-05-29 22:12:23 +0800

    added dockerfile

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        My website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -100,12 +100,12 @@ export default function SignUp() {
                   autoComplete="new-password"
                 />
               </Grid>
-              <Grid item xs={12}>
+              {/* <Grid item xs={12}>
                 <FormControlLabel
                   control={<Checkbox value="allowExtraEmails" color="primary" />}
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
-              </Grid>
+              </Grid> */}
             </Grid>
             <Button
               type="submit"
@@ -117,7 +117,7 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link to="/sign-in.js" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
